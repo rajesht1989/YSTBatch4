6cdb8cfa0eaf275a1150b09868858088f5bc8b62
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-03-31 15:23:07 +0530

    Create The wolf\n.c

---
+++ The wolf\n.c
@@ -0,0 +1,12 @@
+// print the The wolf four times on console
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("The wolf\n");
+    printf("The wolf\n");
+    printf("The wolf\n");
+    printf("The wolf\n");
+    return 0;
+}
