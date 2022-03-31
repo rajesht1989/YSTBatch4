57b6c86627a54f245e14537709afce9a67484b48
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-03-31 15:27:18 +0530

    Create Software\n.c

---
+++ Software\n.c
@@ -0,0 +1,10 @@
+//print the software 3times times on console
+#include <stdio.h>
+
+int main() {
+    
+    printf("software\n");
+    printf("software\n");
+    printf("software");
+    return 0;
+}
