a6dc7b52764e45503e1bf462273065c12b400619
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-01 19:06:24 +0530

    Create Character2.c

---
+++ Character2.c
@@ -0,0 +1,8 @@
+// Define the two character and print it on console
+#include <stdio.h>
+void main() {
+    char chr1 = 'k';
+    char chr2 = 'g';
+    printf("character1 = %c \n",chr1);
+    printf("character2 = %c",chr2);
+  }
