e54a7748b9f9e15db4e70175824d1a676eded0aa
7845522988 <102655488+7845522988@users.noreply.github.com>
2022-04-01 11:51:30 +0530

    Create unsigneddecimalvalue.c

---
+++ unsigneddecimalvalue.c
@@ -0,0 +1,7 @@
+//Req : print a number of unsigned decimal values on console 
+#include <stdio.h>
+void main()
+{
+unsigned int decimalValue=-25;
+ printf("what is the decimal value%u", decimalValue);
+ }
