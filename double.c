18efcba6bc319d9e8bfa4c0f5fa2f4923f85d0f5
7845522988 <102655488+7845522988@users.noreply.github.com>
2022-04-01 11:54:13 +0530

    Create double.c

---
+++ double.c
@@ -0,0 +1,7 @@
+//Req : print a number of double decimal values on console 
+#include <stdio.h>
+void main()
+{
+double decimalValue=-0.337749284;
+ printf("what is the decimal value%u", decimalValue);
+ }
