b06389216610d5b526c58bd3c5c16052da76fa28
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-01 18:55:48 +0530

    Create Float2.c

---
+++ Float2.c
@@ -0,0 +1,10 @@
+// Define the two value and print it on console
+#include <stdio.h>
+
+void main() {
+    float number1 = 14.8;
+    float number2 = 22.1;
+    printf("The number1 is %f \n",number1);
+    printf("The number2 is %f",number2);
+   
+}
