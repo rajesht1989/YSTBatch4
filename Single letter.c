03e1d1365dd0bbcee3db7787be200c7a75fe4887
KALIJACK1997 <102751809+KALIJACK1997@users.noreply.github.com>
2022-04-02 10:02:53 +0530

    Create Single letter.c

---
+++ Single letter.c
@@ -0,0 +1,8 @@
+// requirement: write a single letter using format specifier  print it on console
+#include <stdio.h>
+
+void main() {
+   char singleLetter = 'K';
+    printf("singleLetter is %c",singleLetter);
+   
+}
