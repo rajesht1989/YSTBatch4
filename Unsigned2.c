4d3133ed53d01e9748cf7a4e64b410fd386ec797
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-02 09:49:58 +0530

    Create Unsigned2.c

---
+++ Unsigned2.c
@@ -0,0 +1,9 @@
+// Define a two unsigned number and print it on console
+#include <stdio.h>
+void main() {
+   int number1 = 88;
+   int number2 = 78;
+   printf("The number1 is %u \n",number1);
+   printf("The number2 is %u",number2);
+}
+
