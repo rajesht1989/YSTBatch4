25c9745b8b0174d6c8c51066ba3a27b4c6df4da3
Muppidathigs <102712499+Muppidathigs@users.noreply.github.com>
2022-04-02 12:09:02 +0530

    Create GetANumberFromUser.c

---
+++ GetANumberFromUser.c
@@ -0,0 +1,11 @@
+//Get a number from user and print it on console
+#include<stdio.h>
+void main() {
+    int number = getANumberFromUser();
+    printf("Number is%d",number);
+}
+int getANumberFromUser(){
+    int i;
+    scanf("%d",&i);
+    return i;
+}
