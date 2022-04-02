6765bd5f0bbc57b32ffc63c52ed16d2464121f67
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-02 09:53:09 +0530

    Create Double.c

---
+++ Double.c
@@ -0,0 +1,9 @@
+// Define a two long float number and print it on console
+#include <stdio.h>
+void main() {
+   double number1 = 19.356;
+   double number2 = 17.568;
+   printf("The number1 is %lf \n",number1);
+   printf("The number2 is %lf",number2);
+}
+
