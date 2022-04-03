dd6c447ceb5bf41d2fa9207fb5b4c6117eb914a9
KALIJACK1997 <102751809+KALIJACK1997@users.noreply.github.com>
2022-04-03 20:21:55 +0530

    Create Get float no from user.c

---
+++ Get float no from user.c
@@ -0,0 +1,16 @@
+// get decimal number from user and using format specifier print it on console
+#include<stdio.h>
+void main(){
+    float number = getNumberFromUser();
+    printf("Number = %f",number);
+}
+    int getNumberFromUser(){
+        int i;
+        scanf("%f",&i);
+        return i;
+}
+
+Result: 
+  */tmp/qVGGBMr5YF.o
+1.2
+Number = 1067030912.000000
