5e24967703656955a4873237aad05c19e5e3c6a8
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-03 13:40:32 +0530

    Create scanfchar1.c

---
+++ scanfchar1.c
@@ -0,0 +1,11 @@
+// Get a float number from user and print it on console
+#include <stdio.h>
+void main() {
+    char chr = getACharacterFromUser();
+    printf("character is %c",chr);
+}
+ int getACharacterFromUser(){
+     char i; 
+     scanf("%c",&i);
+     return i;
+}
