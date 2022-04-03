1c15d02a259aa441afbb5485ceff02451d421dd6
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-04-03 22:59:59 +0530

    Create PrintNumberFromUser.c

---
+++ PrintNumberFromUser.c
@@ -0,0 +1,11 @@
+// Requirement:Get a number from user & print it on console
+#include <stdio.h>
+void main (){
+int number = getNumberFromUser();
+    printf("Number is %d",number);
+}
+int getNumberFromUser (){
+    int i;
+    scanf("%d",&i);
+    return i;
+}
