4a7b78576cfcfe38c52e50e5be65b7649b7bd737
Rajesh <rajesht1989@gmail.com>
2022-04-04 10:50:45 +0530

    Update scanffloat4.c

--- scanffloat4.c
+++ scanffloat4.c
@@ -1,34 +1,20 @@
 // Get a four float number from user and print it on console
 #include <stdio.h>
 void main() {
-    float number1 = getAFirstFloatNumberFromUser();
-    float number2 = getASecondFloatNumberFromUser();
-    float number3 = getAThirdFloatNumberFromUser();
-    float number4 = getAFourthFloatNumberFromUser();
-    printf("Number is %f \n",number1);
-    printf("Number is %f \n",number2);
-    printf("Number is %f \n",number3);
-    printf("Number is %f",number4);
+    float number1 = getAFloatNumberFromUser();
+    float number2 = getAFloatNumberFromUser();
+    float number3 = getAFloatNumberFromUser();
+    float number4 = getAFloatNumberFromUser();
+    printf("1st Number is %f \n",number1);
+    printf("2nd Number is %f \n",number2);
+    printf("3rd Number is %f \n",number3);
+    printf("4th Number is %f",number4);
 }
- int getAFirstFloatNumberFromUser(){
+ int getAFloatNumberFromUser(){
      float i; 
      scanf("%f",&i);
      return i;
 }
-int getASecondFloatNumberFromUser(){
-    float j;
-    scanf("%f",&j);
-    return j;
-}
-int getAThirdFloatNumberFromUser(){
- float k;
- scanf("%f",&k);
- return k;
-}
-int getAFourthFloatNumberFromUser(){
-    float l;
-    scanf("%f",&l);
-    return l;
-}
+
 
 
