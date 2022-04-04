fe600c26c93e88fec1288f44efb4af3a323e0bd1
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-04 16:48:09 +0530

    Update scanfchar1.c

--- scanfchar1.c
+++ scanfchar1.c
@@ -1,11 +1,13 @@
 // Get a  one character from user and print it on console
 #include <stdio.h>
+ char getACharacterFromUser(){
+     char i; 
+     printf("enter a character ");
+     scanf("%c",&i);
+     return i;
+}
 void main() {
     char chr = getACharacterFromUser();
     printf("character is %c",chr);
 }
- int getACharacterFromUser(){
-     char i; 
-     scanf("%c",&i);
-     return i;
-}
+
