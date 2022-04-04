516af45ad76e438516caffd9dd6a9720b381eaa6
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-04 16:44:06 +0530

    Update scanfchar4.c

--- scanfchar4.c
+++ scanfchar4.c
@@ -1,32 +1,18 @@
 // Get a  four character from user and print it on console
 #include <stdio.h>
+ char getACharacterFromUser(){
+     char i; 
+     printf("enter a character" );
+     scanf("%c",&i);
+     return i;
+}
 void main() {
-    char chr1 = getAFirstCharacterFromUser();
-    char chr2 = getASecondCharacterFromUser();
-    char chr3 = getAThirdCharacterFromUser();
-    char chr4 = getAFourthCharacterFromUser();
+    char chr1 = getACharacterFromUser();
+    char chr2 = getACharacterFromUser();
+    char chr3 = getACharacterFromUser();
+    char chr4 = getACharacterFromUser();
     printf("character is %c \n",chr1);
     printf("character is %c \n",chr2);
     printf("character is %c \n",chr3);
     printf("character is %c",chr4);
 }
- int getAFirstCharacterFromUser(){
-     char i; 
-     scanf("%c",&i);
-     return i;
-}
-int getASecondCharacterFromUser(){
-    char j;
-    scanf("%c",&j);
-    return j;
-}
-int getAThirdCharacterFromUser(){
-    char k;
-    scanf("%c",&k);
-    return k;
-}
-int getAFourthCharacterFromUser(){
-    char l;
-    scanf("%c",&l);
-    return l;
-}    
