ed6890a4a7da46ab82b45537f23204c0b57bea15
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-04 16:18:57 +0530

    Update scanffloat2.c

--- scanffloat2.c
+++ scanffloat2.c
@@ -1,18 +1,16 @@
 // Get a two float number from user and print it on console
 #include <stdio.h>
-void main() {
-    float number1 = getAFirstFloatNumberFromUser();
-    float number2 = getASecondFloatNumberFromUser();
-    printf("Number is %f \n",number1);
-    printf("Number is %f",number2);
-}
- int getAFirstFloatNumberFromUser(){
-     float i; 
+ float getAFloatNumberFromUser(){
+     float i;
+     printf("enter a number" );
      scanf("%f",&i);
      return i;
 }
-int getASecondFloatNumberFromUser(){
-    float j;
-    scanf("%f",&j);
-    return j;
+void main() {
+    float number1 = getAFloatNumberFromUser();
+    float number2 = getAFloatNumberFromUser();
+    printf("Number is %f \n",number1);
+    printf("Number is %f",number2);
 }
+
+
