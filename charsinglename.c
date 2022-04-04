8a1ef7416d2007b02db559cfcb68fa0102ef209a
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-04 08:43:18 +0530

    Create charsinglename.c

---
+++ charsinglename.c
@@ -0,0 +1,14 @@
+// Get a single character from user and print it on console
+#include <stdio.h>
+void main()
+{
+    char name = getANameFromUser();
+    printf("Name is :%c",name);
+}
+char getANameFromUser()
+{
+    char n;
+    printf("Enter a name :");
+    scanf("%c",&n);
+    return c;
+}
