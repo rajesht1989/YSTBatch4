3868f48a05ec08849bd153e672fe3b36e0b9d272
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-04 14:51:12 +0530

    Update scanfint4.c

--- scanfint4.c
+++ scanfint4.c
@@ -1,34 +1,18 @@
 // Get a four number from user and print it on console
 #include <stdio.h>
 void main() {
-    int number1 = getAFirstNumberFromUser();
-    int number2 = getASecondNumberFromUser();
-    int number3 = getAThirdNumberFromUser();
-    int number4 = getAFourthNumberFromUser();
+    int number1 = getANumberFromUser();
+    int number2 = getANumberFromUser();
+    int number3 = getANumberFromUser();
+    int number4 = getANumberFromUser();
     printf("Number is %d \n",number1);
     printf("Number is %d \n",number2);
     printf("Number is %d \n",number3);
     printf("Number is %d",number4);
 }
- int getAFirstNumberFromUser(){
-     int i; 
+ int getANumberFromUser(){
+     int i;
+     printf("Enter a number");
      scanf("%d",&i);
      return i;
 }
-int getASecondNumberFromUser(){
-    int j;
-    scanf("%d",&j);
-    return j;
-}
-int getAThirdNumberFromUser(){
- int k;
- scanf("%d",&k);
- return k;
-}
-int getAFourthNumberFromUser(){
-    int l;
-    scanf("%d",&l);
-    return l;
-}
-
-
