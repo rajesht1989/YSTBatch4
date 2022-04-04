0d4ed8466e9b297203128646d7aace73d5edb6a8
KALIJACK1997 <102751809+KALIJACK1997@users.noreply.github.com>
2022-04-04 07:24:10 +0530

    Create Single letter from user.c

---
+++ Single letter from user.c
@@ -0,0 +1,17 @@
+//// get single letter from user and using format specifier print it on console
+#include<stdio.h>
+void main(){
+    char letter = getSingleLetterFromUser();
+    printf("SingleLetter = %c",letter);
+}
+    int getSingleLetterFromUser(){
+        int i;
+        scanf("%c",&i);
+        return i;
+}
+
+Result:
+   */tmp/hVbuwjA91m.o
+k
+
+SingleLetter = k
