e3f9cb932d5f610775ba37a1ab562b93071ecb3f
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-04 16:13:13 +0530

    Update scanffloat3.c

--- scanffloat3.c
+++ scanffloat3.c
@@ -1,25 +1,16 @@
 // Get a three float number from user and print it on console
 #include <stdio.h>
-void main() {
-    float number1 = getAFirstFloatNumberFromUser();
-    float number2 = getASecondFloatNumberFromUser();
-    float number3 = getAThirdFloatNumberFromUser();
-    printf("Number is %f \n",number1);
-    printf("Number is %f \n",number2);
-    printf("Number is %f",number3);
-}
- int getAFirstFloatNumberFromUser(){
+ float getAFloatNumberFromUser(){
      float i; 
+     printf("enter a number" );
      scanf("%f",&i);
      return i;
 }
-int getASecondFloatNumberFromUser(){
-    float j;
-    scanf("%f",&j);
-    return j;
-}
-int getAThirdFloatNumberFromUser(){
- float k;
- scanf("%f",&k);
- return k;
+void main() {
+    float number1 = getAFloatNumberFromUser();
+    float number2 = getAFloatNumberFromUser();
+    float number3 = getAFloatNumberFromUser();
+    printf("Number is %f \n",number1);
+    printf("Number is %f \n",number2);
+    printf("Number is %f",number3);
 }
