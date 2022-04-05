3089ff888ddb813a068fda466e045e223feae989
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-05 12:52:46 +0530

    Update scanfchar2.c

--- scanfchar2.c
+++ scanfchar2.c
@@ -1,19 +1,17 @@
 // Get a  two character from user and print it on console
 #include <stdio.h>
-void main() {
-    char chr1 = getAFirstCharacterFromUser();
-    char chr2 = getASecondCharacterFromUser();
-    printf("character is %c \n",chr1);
-    printf("character is %c",chr2);
-}
- int getAFirstCharacterFromUser(){
+ char getACharacterFromUser(){
      char i; 
+     printf("Enter a Number ");
      scanf("%c",&i);
      return i;
 }
-int getASecondCharacterFromUser(){
-    char j;
-    scanf("%c",&j);
-    return j;
+void main() {
+    char chr1 = getACharacterFromUser();
+    char chr2 = getACharacterFromUser();
+    printf("character is %c \n",chr1);
+    printf("character is %c",chr2);
 }
+
+
 
