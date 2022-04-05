65da1a80e831128b2687c13dd7bc73e148abefa4
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-05 19:12:54 +0530

    Create FloatDivideRemin.c

---
+++ FloatDivideRemin.c
@@ -0,0 +1,26 @@
+// get two decimal number, division them and print it on console
+
+#include <stdio.h>
+
+float getANumberFromUser() {
+    float number;
+    printf("Enter a number ");
+    scanf("%f", &number);
+    return number;
+}
+
+float divideTwoNumbers(float a, float b) {
+    return a / b;
+}
+float reminderTwoNumbers(float a,float b){
+    return a / b;
+}
+
+void main() {
+    float number1 = getANumberFromUser();
+    float number2 = getANumberFromUser();
+    float division = divideTwoNumbers(number1, number2);
+    float reminder = reminderTwoNumbers(number1,number2);
+    printf("Division is %f \n", division);
+    printf("reminder is%f", Reminder);
+}
