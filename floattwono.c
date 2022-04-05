6b8db6aa8b96d6dda9e67eb897ba1274ae3009ed
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-05 08:28:01 +0530

    Create floattwono.c

---
+++ floattwono.c
@@ -0,0 +1,16 @@
+// Get a two numbers from user and print it on console
+#include <stdio.h>
+float getANumberFromUser()
+{
+    float i;
+    printf("Enter the number :");
+    scanf("%f",&i);
+    return i;
+}
+
+void main()
+{
+    float number1 = getANumberFromUser();
+    float number2 = getANumberFromUser();
+    printf("Number is : %f %f", number1,number2);
+}
