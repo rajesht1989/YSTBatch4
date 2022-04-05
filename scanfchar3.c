2afb114e059a14199040bcefc1229aba807d35bf
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-05 12:48:48 +0530

    Update scanfchar3.c

--- scanfchar3.c
+++ scanfchar3.c
@@ -1,26 +1,16 @@
 // Get a  three character from user and print it on console
 #include <stdio.h>
+ char getACharacterFromUser(){
+     char i;
+     printf("Enter a Character");
+     scanf("%c",&i);
+     return i;
+}
 void main() {
-    char chr1 = getAFirstCharacterFromUser();
-    char chr2 = getASecondCharacterFromUser();
-    char chr3 = getAThirdCharacterFromUser();
+    char chr1 = getACharacterFromUser();
+    char chr2 = getACharacterFromUser();
+    char chr3 = getACharacterFromUser();
     printf("character is %c \n",chr1);
     printf("character is %c \n",chr2);
     printf("character is %c",chr3);
 }
- int getAFirstCharacterFromUser(){
-     char i; 
-     scanf("%c",&i);
-     return i;
-}
-int getASecondCharacterFromUser(){
-    char j;
-    scanf("%c",&j);
-    return j;
-}
-int getAThirdCharacterFromUser(){
-    char k;
-    scanf("%c",&k);
-    return k;
-}
-
