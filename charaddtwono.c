e303b2239c882c515beaa8162a1926026f1aa871
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-06 07:23:26 +0530

    Create charaddtwono.c

---
+++ charaddtwono.c
@@ -0,0 +1,16 @@
+/Req: Get a two character from user add them & print it on console
+#include<studio.h>
+char getATwoCharacterFromUser()
+{
+ char n;
+ printf("Enter a character is :");
+ scanf("%c",&i);
+ return i;
+ }
+ void main ()
+ {
+  char character1 = getATwoCharacterFromUser();
+  char character2 = getATwoCharacterFromUser();
+  float add = character1+character2;
+  printf("Addition value is :",add);
+}
