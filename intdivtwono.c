b4cc1e3915372c01336832f456521bd40df547e2
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-06 07:10:07 +0530

    Create intdivtwono.c

---
+++ intdivtwono.c
@@ -0,0 +1,16 @@
+//Req: Get a two number from user division them & print it on console
+#include<studio.h>
+int getATwoNumberFromUser()
+{
+ int n;
+ printf("Enter a number is :");
+ scanf("%d",&n);
+ return n;
+ }
+ void main ()
+ {
+  int num1 = getATwoNumberFromUser();
+  int num2 = getATwoNumberFromUser();
+  int div = numa+num2;
+  printf("Division value is :",div);
+}
