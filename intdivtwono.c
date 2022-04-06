e05bbae6a8aff682ff9624f3b8069ca81d45a0d5
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-06 10:56:33 +0530

    Update intdivtwono.c

--- intdivtwono.c
+++ intdivtwono.c
@@ -1,16 +1,25 @@
-//Req: Get a two number from user division them & print it on console
+//Req: Get a two number from user remainder of division them & print it on console
 #include<studio.h>
 int getATwoNumberFromUser()
 {
  int n;
- printf("Enter a number is :");
+ printf("Please enter a number :");
  scanf("%d",&n);
  return n;
  }
+int divTwoNumbers(int num1,int num2)
+{
+ return num1/num2;
+}
+int divRemainder(int num1,int num2)
+{
+ return num1%num2;
+}
  void main ()
  {
   int num1 = getATwoNumberFromUser();
   int num2 = getATwoNumberFromUser();
-  int div = num1/num2;
-  printf("Division value is :",div);
+  int div = divTwoNumbers(num1/num2);
+  int rmd = divRemainder (num1%num2);
+  printf("Division value : %d Remainder is : %d",div,rmd);
 }
