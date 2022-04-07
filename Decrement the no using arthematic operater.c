c7385a52d95cef3356666474f49992ccd1987f2b
KALIJACK1997 <102751809+KALIJACK1997@users.noreply.github.com>
2022-04-07 19:44:08 +0530

    Create Decrement the no using arthematic operater.c

---
+++ Decrement the no using arthematic operater.c
@@ -0,0 +1,11 @@
+// write a number and decrement the number using arthematic opretor and print it on console
+
+#include <stdio.h>
+
+void main() {
+   int i = 4;
+   i--;
+   printf("%d\n",i);
+   --i;
+   printf("%d",i);
+}
