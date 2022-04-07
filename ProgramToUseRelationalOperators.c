206e7d1ee516a635149c01d5e362dbbbb9630cc8
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-04-07 23:09:51 +0530

    Create ProgramToUseRelationalOperators.c

---
+++ ProgramToUseRelationalOperators.c
@@ -0,0 +1,13 @@
+// Requirement:write a program to use an Relational operators
+#include <stdio.h>
+int main(){
+    int b = 19;
+    int p = 5;
+    printf("b == p %d\n", b==p);
+    printf("b > p %d\n", b>p);
+    printf("b >= p %d\n", b>=p);
+    printf("b <= p %d\n", b<=p);
+    printf("b < p %d\n", b<p);
+    printf("b != p %d\n", b!=p);
+    return 0;
+}
