51afa922dcfb3d2d7c2bbcd34d70af200241a2d0
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-07 13:33:27 +0530

    Create Arithmeticopt.c

---
+++ Arithmeticopt.c
@@ -0,0 +1,15 @@
+// write a program that uses of arithmetic opreators
+#include<stdio.h>
+int main() {
+ int a = 4, b = 8,c;
+ c = a + b;
+ printf("Adddition is %d \n",c);
+ c = a - b;
+ printf("Subtrction is %d \n",c);
+ c = a * b;
+ printf("Multliplication is %d \n",c);
+ c = a / b;
+ printf("Quotient is %d \n",c);
+ c = a % b;
+ printf("Reminder is %d",c);
+}
