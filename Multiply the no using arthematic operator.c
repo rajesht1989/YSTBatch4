6d532844797e1b1067b684089884fec7da922673
KALIJACK1997 <102751809+KALIJACK1997@users.noreply.github.com>
2022-04-07 20:04:44 +0530

    Create Multiply the no using arthematic operator.c

---
+++ Multiply the no using arthematic operator.c
@@ -0,0 +1,8 @@
+// write a no & multiply the number using arthematic operater and print it on console
+# include <stdio.h>
+int main (){
+    int i = 4;
+    i = i*i;
+    printf("%d",i);
+    return 1;
+}
