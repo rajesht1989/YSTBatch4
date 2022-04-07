d3c1753570e013551affca33cf1455fb035c99ea
KALIJACK1997 <102751809+KALIJACK1997@users.noreply.github.com>
2022-04-07 21:34:19 +0530

    Create Using Relation operters.c

---
+++ Using Relation operters.c
@@ -0,0 +1,20 @@
+// write a two value and find a relation operaters and print on console
+#include <stdio.h>
+
+void main() {
+    int a = 6;
+    int b = 8;
+    a==b;
+   printf("%d\n",a==b);
+   a<b;
+   printf("%d\n",a<b);
+   a>b;
+   printf("%d\n",a>b);
+   a<=b;
+   printf("%d\n",a<=b);
+   a>=b;
+   printf("%d\n",a>=b);
+   a!=b;
+    printf("%d",a!=b);
+   
+}
