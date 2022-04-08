7fad7f28c1835a73e46c6f651906c05c114798b8
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-08 09:47:27 +0530

    Update relational.c

--- relational.c
+++ relational.c
@@ -2,22 +2,22 @@
 #include<stdio.h>
 int main(){
 int a = 10,b = 4;
- a==b;
+ 
  printf("Equal are : %d\n",a==b);
  
- a>b;
+ 
  printf("Createrthen are : %d\n",a>b);
  
- a<b;
+
  printf("Lessthan are : %d\n",a<b);
  
- a>=b;
+ 
  printf("Createrthan equal are : %d\n",a>=b);
  
- a<=b;
+
  printf("Lessthan equal are : %d\n",a<=b);
  
- a!=b;
+
  printf("Not equal are : %d\n",a!=b);
  
  return 0;
