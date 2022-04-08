1dc2ae4cd365571da0f787cb78232ff9d583c41e
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-08 08:56:42 +0530

    Create logical.c

---
+++ logical.c
@@ -0,0 +1,15 @@
+//write a program that uses logical operations of them & print it on console
+#include<stdio.h>
+int main(){
+int a = 10,b = 4;
+ a&&b;
+ printf("AND function is : %d\n",a&&b);
+ 
+ a||b;
+ printf("OR function is : %d\n",a||b);
+ 
+ a!b;
+ printf("Not function is : %d\n",a!b);
+ 
+ return 0;
+}
