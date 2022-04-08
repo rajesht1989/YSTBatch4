e97af6cd46d6cbbba527fb298f951f06fad366c9
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-08 15:42:22 +0530

    Create Assignmentoperator.c

---
+++ Assignmentoperator.c
@@ -0,0 +1,25 @@
+// write a program that uses of assignment opertors
+#include<stdio.h>
+void main(){
+    int a =6, b = 12;
+    addAndPrint(a,b);
+    subtractAndPrint(a,b);
+    productAndPrint(a,b);
+    quotientAndPrint(a,b);
+    reminderAndPrint(a,b);
+}
+void addAndPrint(int a, int b){
+    printf("Addition is %d \n",a += b);
+}
+void subtractAndPrint(int a,int b){
+   printf("Subtraction is is %d \n",a -= b); 
+}
+void productAndPrint(int a,int b){
+    printf("Multiplication is is %d \n",a *= b);
+}
+void quotientAndPrint(int a,int b){
+    printf("Quotient is is %d \n",a /= b);
+}
+void reminderAndPrint(int a,int b){
+    printf("Reminder is is %d",a %= b);
+}
