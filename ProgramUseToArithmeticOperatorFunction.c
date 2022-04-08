0d60b833ee1e543278821b05e81bc0c17087bc89
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-04-08 10:59:37 +0530

    Create ProgramUseToArithmeticOperatorFunction.c

---
+++ ProgramUseToArithmeticOperatorFunction.c
@@ -0,0 +1,8 @@
+//Requirement:write a program them arithmetic operator print it on console
+#include <stdio.h>
+void addAndprint(a,b){
+    printf("Addition is %d",a+b);
+}
+void main(){
+    addAndprint(19,5);
+}
