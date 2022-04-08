ffd66b7dcb8dd1341797c1167c5d5481c38986f5
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-08 08:39:34 +0530

    Create relational.c

---
+++ relational.c
@@ -0,0 +1,24 @@
+//write a program that uses relational operations of them & print it on console
+#include<stdio.h>
+int main(){
+int a = 10,b = 4;
+ a==b;
+ printf("Equal are : %d\n",a==b);
+ 
+ a>b;
+ printf("Createrthen are : %d\n",a>b);
+ 
+ a<b;
+ printf("Lessthan are : %d\n",a<b);
+ 
+ a>=b;
+ printf("Createrthan equal are : %d\n",a>=b);
+ 
+ a<=b;
+ printf("Lessthan equal are : %d\n",a<=b);
+ 
+ a!=b;
+ printf("Not equal are : %d\n",a!=b);
+ 
+ return 0;
+}
