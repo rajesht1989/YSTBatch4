87e48b4e340fc5a977c9349123c27971100a264c
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-08 09:46:05 +0530

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -2,19 +2,19 @@
 #include<stdio.h>
 int main(){
 int a = 10,b = 4;
- a+=b;
+
  printf("Addition is : %d\n",a+=b);
  
- a-=b;
+
  printf("Subraction is : %d\n",a-=b);
  
- a*=b;
+ 
  printf("Multiplication is : %d\n",a*=b);
  
- a/=b;
+ 
  printf("Division is : %d\n",a/=b);
  
- a%=b;
+ 
  printf("remainder is : %d\n",a%=b);
  
  return 0;
