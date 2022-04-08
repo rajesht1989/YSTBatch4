c8ff7fe38d5f3f95c8ed81d808658915c36d2290
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-08 08:10:06 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,21 @@
+//write a program that uses arithmetic operations of them & print it on console
+#include<stdio.h>
+int main(){
+int a = 10,b = 4;
+ a+b;
+ printf("Addition is : %d\n",a+b);
+ 
+ a-b;
+ printf("Subraction is : %d\n",a-b);
+ 
+ a*b;
+ printf("Multiplication is : %d\n",a*b);
+ 
+ a/b;
+ printf("Division is : %d\n",a/b);
+ 
+ a%b;
+ printf("remainder is : %d\n"a%b);
+ 
+ return 0;
+}
