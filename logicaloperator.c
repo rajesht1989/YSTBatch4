1de0041012ae4806648431a48a71251e23b9363d
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-08 16:55:28 +0530

    Create logicaloperator.c

---
+++ logicaloperator.c
@@ -0,0 +1,20 @@
+// write a program uses of logical operato
+#include<stdio.h>
+int main () {
+    int a = 4, b = 8, c = 4;
+   int AndGate = (a == c) && (b > c);
+    int OrGate = (c == a) || (a > b);
+    int NotGate = ! (a == c);
+    printf("AND is %d\n",AndGate);
+    printf("OR is %d \n",OrGate);
+    printf("NOT is %d",NotGate);
+}
+ int andGateIs(int a,int b,int c){
+     return 0;
+ }
+ int orGateIs(int a,int b, int c){
+     return 0;
+ }
+ int notGateIs(int a,int c){
+     return 0;
+ }
