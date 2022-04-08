768f6dbc1f530221430b6bf8c8a7ccfd88ca2e9a
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-08 09:43:23 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,12 +1,11 @@
 //Req : int a= 5, int=6 is swapping the assign the value & it on console 
 #include <stdio.h>
 int main(){
-int a = 5,b= 6,temb;
- printf("The online value%d %d\n",a,b);
- scanf("%d %d",&a,&b);
- temb = a;
+int a = 5,b= 6,temp;
+ printf("The original value %d %d\n",a,b);
+ temp = a;
  a = b;
- b = temb;
+ b = temp;
  printf("The swapping value :%d %d",a,b);
  return 0;
 }
