0b88f65b3792c07034e49b1b0248c1803e4478e2
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-04-08 09:51:13 +0530

    Update ProgramUseToLogicalOperators.c

--- ProgramUseToLogicalOperators.c
+++ ProgramUseToLogicalOperators.c
@@ -1,10 +1,10 @@
-// Requirement:Program use to logical operators
+// Write a program to use an logical operator
 #include <stdio.h>
-void main(){
-    int b,p;
-    printf("Enter values for b and p");
-    scanf("%d , %d",(&b,&p));
-    printf("\n %d",(b<p)&&(b!=p));
-    printf("\n %d",(b<p)||(b<p));
-    printf("\n %d",!(b==p));
+int main() {
+    int b=19,p=5;
+    printf("%d \n",b&&p);
+    printf("%d \n",b||p);
+    printf("%d",!b);
+    
+    return 0;
 }
