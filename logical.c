dc6a543c4adfc6b6dfda9388db649198e380e62c
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-08 09:50:28 +0530

    Update logical.c

--- logical.c
+++ logical.c
@@ -8,8 +8,11 @@ int a = 10,b = 4;
  a||b;
  printf("OR function is : %d\n",a||b);
  
- a!b;
- printf("Not function is : %d\n",a!b);
+ !a;
+ printf("Not function is : %d\n",!a);
+ 
+ !b;
+ printf("Not function is : %d\n",!b);
  
  return 0;
 }
