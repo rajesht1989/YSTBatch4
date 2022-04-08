48f1b77093ad0f1fa7fdd56772a365e0c1ea9fc4
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-08 16:25:30 +0530

    Create Relationaloperator.c

---
+++ Relationaloperator.c
@@ -0,0 +1,29 @@
+// write a program that uses of Relational program
+#include<stdio.h>
+int main(){
+    int a =5, b = 5, c = 8;
+    printf("EqualTo is %d \n",a == b);
+    printf("greaterThan is %d \n",b > c);
+    printf("lessThan is %d \n",b < c);
+    printf("notEqualTo is %d \n",a != b);
+    printf("greaterThanOrEqualTo %d \n",c >= a);
+    printf("lessThanOrEqualTo %d",a <= c);
+}
+int EqualTo(int a, int b){
+    return 0;
+}    
+int greaterThan(int b, int c){
+    return 0;
+}
+int lessThan(int b, int c){
+    return 0;
+}
+int notEqualTo(int a,int b){
+    return 0;
+}
+int greaterThanOrEqualTo(int c,int a){
+    return 0;
+} 
+int lessThanOrEqualTo(int a,int c){
+return 0;
+}
