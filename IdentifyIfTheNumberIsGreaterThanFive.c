672e3d36c4c782b4be6744c637f9f50505025261
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-04-09 06:44:47 +0530

    Create IdentifyIfTheNumberIsGreaterThanFive.c

---
+++ IdentifyIfTheNumberIsGreaterThanFive.c
@@ -0,0 +1,20 @@
+// Requirement: get the number from user and identify if the number is greater than five 
+#include<stdio.h>
+int main(){
+    int number = getANumberFromUser();
+    checkIfTheNumber(number);
+    return 0;
+}
+    int getANumberFromUser(){
+    int number;
+    printf("Enter a Number ");
+    scanf("%d",&number);
+    return number;
+    }
+    void checkIfTheNumber(int number){
+    if(number>5){
+        printf("That is Right");
+    }else{
+        printf("That is wrong");
+    }
+}
