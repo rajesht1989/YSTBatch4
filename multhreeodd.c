7fb9e119490e76062f7e9016485ff5b5839757fa
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-17 23:03:04 +0530

    Create multhreeodd.c

---
+++ multhreeodd.c
@@ -0,0 +1,33 @@
+// Get a three number from user and multiple them identify if number is odd number 
+#include<stdio.h>
+int getANumberFromUser()
+{
+    int number;
+    printf("Please enter the number :");
+    scanf("%d",&number);
+    return number;
+}
+int multipleThreeNumberFormUser(int n1,int n2,int n3)
+{
+    return(n1*n2*n3);
+}
+void checkTheNumberIsOddNumber(int mul)
+{
+ if(mul%=0)
+ {
+  printf("The number is Odd number");
+}
+    else
+{
+    printf("The number is Even number");
+}
+}
+int main ()
+{
+    int n1 = getANumberFromUser();
+    int n2 = getANumberFromUser();
+    int n3 = getANumberFromUser();
+    int mul = multipleThreeNumberFormUser(n1,n2,n3);
+    checkTheNumberIsOddNumber(mul);
+    return 0;
+}
