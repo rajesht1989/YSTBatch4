9cc61a638e8edfb9d16c0ebeadb50a0a799872e0
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-17 22:46:01 +0530

    Create multwoodd.c

---
+++ multwoodd.c
@@ -0,0 +1,32 @@
+// Get a two number from user and multiple them identify if number is odd number 
+#include<stdio.h>
+int getANumberFromUser()
+{
+    int number;
+    printf("Please enter the number :");
+    scanf("%d",&number);
+    return number;
+}
+int multipleTwoNumberFormUser(int n1,int n2)
+{
+    return(n1*n2);
+}
+void checkTheNumberIsOddNumber(int mul)
+{
+ if(mul%=0)
+ {
+  printf("The number is Odd number");
+}
+    else
+{
+    printf("The number is Even number");
+}
+}
+int main ()
+{
+    int n1 = getANumberFromUser();
+    int n2 = getANumberFromUser();
+    int mul = multipleTwoNumberFormUser(n1,n2);
+    checkTheNumberIsOddNumber(mul);
+    return 0;
+}
