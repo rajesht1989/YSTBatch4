3efef68910e8692249f196c88b7981c786fd08de
Krishnan14 <100073879+Krishnan14@users.noreply.github.com>
2022-04-18 15:57:02 +0530

    Create getTwoNumberAndAddAndidentifyOddorEven.c

---
+++ getTwoNumberAndAddAndidentifyOddorEven.c
@@ -0,0 +1,28 @@
+// get a two number from user and add them if it is identify the even number
+#include<stdio.h>
+
+int getANumberFromUser(){
+    int number;
+    printf("Enter a number ");
+    scanf("%d",&number);
+    return number;
+}
+int addTwoNumberFromUser(int a,int b){
+  return (a + b) ;
+}
+void identifyTheNumbers(int a,int b){
+    if((a + b) % 2 == 0){
+        printf("Even number");
+    } else {
+        printf("Odd number");
+    }
+    
+}
+int main(){
+    int number1 = getANumberFromUser();
+    int number2 = getANumberFromUser();
+    int addition = addTwoNumberFromUser(number1,number2);
+    printf("Addition is %d \n",addition);
+    identifyTheNumbers(number1,number2);
+    return 0;
+}
