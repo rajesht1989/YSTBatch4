cdd5bde783ba9fad337e1f09a0a03f783a8a0256
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-21 16:52:35 +0530

    Create Verharstar.c

---
+++ Verharstar.c
@@ -0,0 +1,17 @@
+// Write a program for vertical and horizontal print the five star it on console
+#include <stdio.h>
+
+void printTheStarVerticleAndHarizontalOrder(int n)
+{
+    for(int i = 0;i<5;i++){
+  printf ("*\v");   
+ 
+     printf ("*\t");
+ }
+ }
+
+void main()
+{
+ int n = 5;
+ printTheStarVerticleAndHarizontalOrder(n);
+}
