a8b69216b6add7d22b8b76e177967e3c1fa32363
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-04-22 11:00:58 +0530

    Create DrawMatrixNumber.c

---
+++ DrawMatrixNumber.c
@@ -0,0 +1,20 @@
+// get a number from user and draw matrix number 
+#include <stdio.h>
+int getANumberFromUser(){
+    int number;
+    printf("Enter a Number ");
+    scanf("%d", &number);
+    return number;
+}
+void drawMatrix(int n){
+    for(int i = 0; i<n; i++){
+       for(int j = 0; j<n; j++){
+         printf("%d%d\t",i,j);
+    }
+    printf("\n");
+    }
+}
+void main(){
+ drawMatrix(getANumberFromUser());
+ return 0;
+}
