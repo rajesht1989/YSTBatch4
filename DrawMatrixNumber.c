8ed0cd6c59c568d1dc92aa9d52e5755f272bf316
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-04-22 11:01:50 +0530

    Update DrawMatrixNumber.c

--- DrawMatrixNumber.c
+++ DrawMatrixNumber.c
@@ -9,7 +9,7 @@ int getANumberFromUser(){
 void drawMatrix(int n){
     for(int i = 0; i<n; i++){
        for(int j = 0; j<n; j++){
-         printf("%d%d\t",i,j);
+         printf("%d,%d \t",i,j);
     }
     printf("\n");
     }
