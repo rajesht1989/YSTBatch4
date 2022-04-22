93cca1bd3c96d3b30ab92489cad22dbfcffecbd4
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-22 08:26:20 +0530

    Create thosemanynumber.c

---
+++ thosemanynumber.c
@@ -0,0 +1,21 @@
+// Get a number from user and those many number to print it on console
+ #include <stdio.h> 
+ 
+ int getNumberFromUser() {
+     int i;
+     printf("Please enter a number ");
+     scanf("%d", &i);
+     return i;
+     }
+     
+void thoseManyNumberFromUser(int n) {
+        for(int i = 0; i < n; i++){
+            int num = getNumberFromUser();
+            scanf("%d",&num);
+        return num;
+    }
+}
+
+void main() {
+    thoseManyNumberFromUser(getNumberFromUser());
+}
