41c212a4972feebd2847d8304bbead943e5156ef
KamarajRK <102655488+7845522988@users.noreply.github.com>
2022-04-22 09:57:35 +0530

    Update starrectangle.c

--- starrectangle.c
+++ starrectangle.c
@@ -10,14 +10,13 @@
      
 void drawRectangleFor1Or2(int n) {
     for (int j = 0; j < n; j++) {
-        for(int i = 0; i < n; i++){
-            if(i == n+n){
+        for(int i = 0; i < n*2; i++){
             printf("* \t");
         } 
         printf("\n");
     }
 }
-}
+
 
 void main() {
     drawRectangleFor1Or2(getNumberOfWindows());
