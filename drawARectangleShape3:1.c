6c58b1c0633d18335232cb21c4604154ba69a403
Narasingam-marimuthu <102815500+Narasingam-marimuthu@users.noreply.github.com>
2022-04-22 10:27:23 +0530

    Create drawARectangleShape3:1.c

---
+++ drawARectangleShape3:1.c
@@ -0,0 +1,29 @@
+// get a number from user and draw the rectangle shape 3:1
+
+/* algorithm 
+
+step 1 : Write a function to get number from user using scanf statement
+step 2 : write a function to draw a rectangle shape using looping statement
+        - write a looping statement to rectangle column
+        - write a looping statement to rectangle row
+step 3 : write a main function to invoke the get number from user function and draw the rectangle shape function
+*/
+#include <stdio.h>
+int getNumberFromUser(){
+    int number;
+    printf("Enter The Rectangle Shape: ");
+    scanf("%d",&number);
+    return number;
+}
+void drawARectangle(int a){
+    for(int i=0;i<a*3;i++){
+        for(int j=0;j<a;j++){
+            printf("* ");
+        }
+        printf("*\n");
+    }
+}
+void main (){
+    drawARectangle(getNumberFromUser());
+    
+}
