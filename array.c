aee949fea266aa8adaec5bd5ab2340c4ff9ecb90
Narasingam-marimuthu <102815500+Narasingam-marimuthu@users.noreply.github.com>
2022-04-28 14:28:49 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,12 @@
+// Write a program define array print second array and change third array
+#include <stdio.h>
+
+int array(){
+    int number [7] = {67,54,37,28,12,75,67};
+    printf("%d \n",number[2]);
+    number [3] = 50;
+    printf("%d",number[3]);
+}
+void main(){
+    array();
+}
