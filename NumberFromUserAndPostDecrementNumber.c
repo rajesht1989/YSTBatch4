a4415099db4a66ce14a85153f92dc648b61a4cb3
Georgebala007 <101312564+Georgebala007@users.noreply.github.com>
2022-08-10 15:26:40 +0530

    Create NumberFromUserAndPostDecrementNumber.c

---
+++ NumberFromUserAndPostDecrementNumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int getANumberFromUser(){
+    int n;
+    printf("Enter a number ");
+    scanf("%d",&n);
+    return n;
+}
+int incrementNumber(int n){
+    return n--;
+}
+int main(){
+    int n=getANumberFromUser();
+    printf("%d\n",n--);
+    return n;
+}
