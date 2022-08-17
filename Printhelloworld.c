01439cc37871f53075167a9d999d4ae98b0f2a59
Rajaganesan1994 <102713769+Rajaganesan1994@users.noreply.github.com>
2022-08-17 14:59:32 +0530

    Create Printhelloworld.c

---
+++ Printhelloworld.c
@@ -0,0 +1,11 @@
+// write a program to ur name 5 times print it on console
+
+#include<stdio.h>
+int main() {
+    printf ("rajaganesh\n");
+    printf ("rajaganesh\n");
+    printf ("rajaganesh\n");
+    printf ("rajaganesh\n");
+    printf ("rajaganesh");
+    return 0;
+}
